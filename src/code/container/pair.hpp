e5b418adc32a3dab9a925a6750543e2e8dadbb72
Jeb Feng <ranzeplay@outlook.com>
2021-02-10 18:03:14 +0800

    feat (container/pair) : Add KeyValuePair

---
+++ src/code/container/pair.hpp
@@ -0,0 +1,16 @@
+namespace staplerio
+{
+	namespace libcpp
+	{
+		namespace container
+		{
+			template <typename TKey, typename TValue>
+			class KeyValuePair
+			{
+			public:
+				TKey key;
+				TValue value;
+			};
+		} // namespace container
+	}	  // namespace libcpp
+} // namespace staplerio
